94941f0ce6c90a7acc59973250a6661dd854389c
Fabian Schillig <xorgmc@gmail.com>
2025-02-21 01:07:54 +0100

    Added packages, fehbg

--- install-packages.sh
+++ install-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt install stow i3 polybar rofi picom
+sudo apt install stow i3 polybar rofi picom alttab brightnessctl pamixer feh
 
 echo "Run \"stow .\" in this (dotfiles) folder!"
