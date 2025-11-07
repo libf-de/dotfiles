f54f7d5a2400c205a222dc43aab9933cb2b74381
Fabian Schillig <fabian@libf.de>
2025-11-07 01:14:06 +0100

    Ignore unneeded repo files

--- install-packages.sh
+++ install-packages.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  brew install zoxide fzf stow
+  exit 0
+fi
+
+# Don't link Library on Linux
+grep Library .stow-local-ignore >/dev/null || echo "Library" >> .stow-local-ignore
+
 sudo apt install stow i3 polybar rofi picom alttab brightnessctl pamixer feh zoxide fzf xscreensaver
 
 echo "Run \"stow .\" in this (dotfiles) folder!"
